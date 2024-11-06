ea04a32093cc122679bac72354a4531c9991fc15
tyokousagi <tyokousagi20022@gmail.com>
2024-11-06 11:28:46 +0000

    first

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String test() {
+		return "index";
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+
+	@GetMapping("/submit")
+	public String submit() {
+		return "submit";
+	}
+}
